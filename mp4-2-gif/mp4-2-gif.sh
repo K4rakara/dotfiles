59b77c1c96d7742ac556f72fd53c265934245ef3
K4rakara <jack@insertdomain.name>
2020-06-10 18:09:43 -0400

    Add mp4-2-gif util

---
+++ mp4-2-gif/mp4-2-gif.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+ffmpeg \
+	-i `echo $1` \
+	-r `mediainfo --fullscan $1 | jsgrep "(?<=Frame count                              : )\d+"` \
+	-vf "scale=`mediainfo --fullscan $1 | jsgrep "(?<=Height                                   : )\d+(?= pixels)"`:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+	-ss 00:00:00 -to `mediainfo --fullscan ./0u1rkq805x351.gif | jsgrep "(?<=Duration                                 : )\d\d:\d\d:\d\d.\d+" | round-time-up` \
+	$2
