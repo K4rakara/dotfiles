119f5bd012924be12ef6aed13c77aff963f84c56
K4rakara <jack@insertdomain.name>
2020-06-12 18:38:37 -0400

    Update mp4-2-gif

--- mp4-2-gif/mp4-2-gif.sh
+++ mp4-2-gif/mp4-2-gif.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/bash
-ffmpeg \
-	-i `echo $1` \
-	-r `mediainfo --fullscan $1 | jsgrep "(?<=Frame count                              : )\d+"` \
-	-vf "scale=`mediainfo --fullscan $1 | jsgrep "(?<=Height                                   : )\d+(?= pixels)"`:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
-	-ss 00:00:00 -to `mediainfo --fullscan $1 | jsgrep "(?<=Duration                                 : )\d\d:\d\d:\d\d.\d+" | round-time-up` \
-	$2
+printHelp() {
+echo "
+mp4-2-gif v0.0.1
+
+Usage:
+
+mp4-2-gif [input file.mp4] [output file.gif]
+"
+}
+
+if [ "$1" != "" ]
+then
+	if ["$2" != ""]
+	then
+		ffmpeg \
+			-i `echo $1` \
+			-r `mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Frame count                              : )\d+"` \
+			-vf "scale=`mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Height                                   : )\d+(?= pixels)"`:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+			-ss 00:00:00 -to `mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Duration                                 : )\d\d:\d\d:\d\d.\d+" | round-time-up` \
+			$2		
+	else
+		printHelp
+	fi
+else
+	printHelp
+fi
