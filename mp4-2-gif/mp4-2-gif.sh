12e97558f37ff470d70a1afdb0150981704d851c
K4rakara <jack@insertdomain.name>
2020-06-12 20:24:56 -0400

    Bugfix

--- mp4-2-gif/mp4-2-gif.sh
+++ mp4-2-gif/mp4-2-gif.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+
+frameCount="0"
+height="1024"
+duration="00:00:00"
+
 printHelp() {
 echo "
 mp4-2-gif v0.0.1
@@ -9,15 +14,34 @@ mp4-2-gif [input file.mp4] [output file.gif]
 "
 }
 
+getFrameCount() {
+	frameCount=`mediainfo --fullscan $1 | jsgrep -o --color=none -m 1 "(?<=Frame count                              : )\d+"`
+}
+
+getHeight() {
+	tmpHeight=`mediainfo --fullscan $1 | jsgrep -o --color=none -m 1 "(?<=Height                                   : )\d+"`
+	if [ -z "$tmpHeight" ]
+	then
+		height="$tmpHeight"
+	fi
+}
+
+getDuration() {
+	duration=`mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Duration                                 : )\d\d:\d\d:\d\d.\d+" | round-time-up`
+}
+
 if [ "$1" != "" ]
 then
 	if [ "$2" != "" ]
 	then
+		getFrameCount $1
+		getHeight $1
+		getDuration $1
 		ffmpeg \
 			-i `echo $1` \
-			-r `mediainfo --fullscan $1 | jsgrep -o --color=none -m 1 "(?<=Frame count                              : )\d+"` \
-			-vf "scale=`mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Height                                   : )\d+(?= pixels)"`:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
-			-ss 00:00:00 -to `mediainfo --fullscan $1 | jsgrep -o --color=none "(?<=Duration                                 : )\d\d:\d\d:\d\d.\d+" | round-time-up` \
+			-r $frameCount \
+			-vf "scale=$height:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+			-ss 00:00:00 -to $duration \
 			$2
 	else
 		printHelp
