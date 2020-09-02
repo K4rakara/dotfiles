361ff6607523fd42aba1b4c4fe2b6e3c0bdf994f
Jack Johannesen <jack@insertdomain.name>
2020-09-02 15:50:26 -0400

    Fix Vim config

--- .vim/.link.sh
+++ .vim/.link.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
-ln -s -f $PWD/.vim ~/.vim;
+
+[ -d ~/.vim/ ] || mkdir -p ~/.vim/;
+
+[ -f $PWD/.vim/autoload/plug.vim ] || curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > $PWD/.vim/autoload/plug.vim;
+
+ln -s -f $PWD/.vim/autoload ~/.vim/autoload;
 
